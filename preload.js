b739c28d527fdd486e355c59c238151a2308898f
thething2442 <developmentserver160@gmail.com>
2025-10-05 12:07:18 +0000

    added

--- preload.js
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron'
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  send: (channel, data) => ipcRenderer.send(channel, data),
-  receive: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
-});
